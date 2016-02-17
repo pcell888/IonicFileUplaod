1ac3b12588be48def7c3eefdff4a8b257718cf3d
rahul <rohngonnarock>
2016-02-17 22:59:00 +0530

    Running project udpated

--- www/js/app.js
+++ www/js/app.js
@@ -1,140 +1,73 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'angularFileUpload'])
+angular.module('starter', ['ionic', 'firebase', 'starter.controllers','ngCordova', 'angularFileUpload'])
 
     .run(function ($ionicPlatform) {
         $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
             if (window.cordova && window.cordova.plugins.Keyboard) {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-                // Don't remove this line unless you know what you are doing. It stops the viewport
-                // from snapping when text inputs are focused. Ionic handles this internally for
-                // a much nicer keyboard experience.
                 cordova.plugins.Keyboard.disableScroll(true);
+
             }
             if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
                 StatusBar.styleDefault();
             }
         });
     })
-    .controller('UploadCtrl', function ($scope, $http, $timeout, $upload) {
-
-        $scope.upload = [];
-        $scope.fileUploadObj = { testString1: "Test string 1", testString2: "Test string 2" };
-
-        $scope.onFileSelect = function ($files) {
-            //$files: an array of files selected, each file has name, size, and type.
-            for (var i = 0; i < $files.length; i++) {
-                var $file = $files[i];
-                (function (index) {
-                    $scope.upload[index] = $upload.upload({
-                        url: "https://microsoft-apiapp55759bba47b74474bffa45d9538d840b.azurewebsites.net/api/files/upload", // webapi url
-                        method: "POST",
-                        data: { fileUploadObj: $scope.fileUploadObj },
-                        file: $file
-                    }).progress(function (evt) {
-                        // get upload percentage
-                        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-                    }).success(function (data, status, headers, config) {
-                        // file is uploaded successfully
-                        console.log('Success');
-                        console.log(data);
-                    }).error(function (data, status, headers, config) {
-                        // file failed to upload
-                        console.log('Failure');
-                        console.log(data);
-                    });
-                })(i);
-            }
+    .factory("Items", ["$firebaseArray",
+        function ($firebaseArray) {
+            var ref = new Firebase("https://ringtoneapp.firebaseio.com/ringtones");
+            return $firebaseArray(ref);
         }
+    ])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-        $scope.abortUpload = function (index) {
-            $scope.upload[index].abort();
-        }
-    })
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppCtrl'
+            })
 
+            .state('app.search', {
+                url: '/search',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/search.html'
+                    }
+                }
+            })
 
-    .controller('DownloadController', function ($scope, $http, $timeout, $upload) {
+            .state('app.browse', {
+                url: '/browse',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/browse.html',
+                        controller:'BrowseCtrl'
+                    }
+                }
+            })
+            .state('app.playlists', {
+                url: '/playlists',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/playlists.html',
+                        controller: 'PlaylistsCtrl'
+                    }
+                }
+            })
 
-        $scope.download = function () {
-            debugger;
-            // $http({
-            //     method: 'GET',
-            //     url: 'https://microsoft-apiapp55759bba47b74474bffa45d9538d840b.azurewebsites.net/api/download',
-            //     headers: {
-            //         //'Accept': 'audio/mpeg',
-            //         'Accept': 'application/json',
-            //        // 'Content-Disposition': 'attachment; filename=foo.mp3'
-            //     },
-            //     responseType: 'arraybuffer'
-            // }).then(function (data, status, headers, config) {
-            //     var blob = new Blob([data], { type: "audio/mpeg" });
-            //     var objectUrl = URL.createObjectURL(blob);
-            //     console.log('Success' + objectUrl);
-            //     window.open(objectUrl);
-            // }, function (error) {
-            //     console.log(error);
-            // });
-            
-            
-            $http({
-                method: 'get',
-                responseType: 'arraybuffer',
-                //url: 'https://microsoft-apiapp55759bba47b74474bffa45d9538d840b.azurewebsites.net/api/download'
-                url: "https://ringblob.blob.core.windows.net/ringtones/MyTone%20-16.%20The%20Weeknd%20-%20Can't%20Feel%20My%20Face.mp3",
-            }).then(function successCallback(response) {
-                // this callback will be called asynchronously
-                console.log(response);
-
-                var blob = new Blob([response.data], {
-                    type: 'application/json'
-                });
-                var blobURL = (window.URL || window.webkitURL).createObjectURL(blob);
-                var anchor = document.createElement("a");
-                anchor.download = "MyTone -16. The Weeknd - Can't Feel My Face.mp3";
-                anchor.href = blobURL;
-                anchor.click();
-                // when the response is available
-            }, function errorCallback(response) {
-                console.log(response);
-     
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
+            .state('app.single', {
+                url: '/playlists/:playlistId',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/playlist.html',
+                        controller: 'PlaylistCtrl'
+                    }
+                }
             });
-            
-            
-            //window.location.href = 'https://microsoft-apiapp55759bba47b74474bffa45d9538d840b.azurewebsites.net/api/download';
-            //$http({ method: 'GET', url: 'https://microsoft-apiapp55759bba47b74474bffa45d9538d840b.azurewebsites.net/api/download' }).success(function (data, status, headers, config) {
-            //    //var anchor = angular.element('<a/>');
-            //    //anchor.attr({
-            //    //    href: 'data:attachment/mp3;charset=utf-8,' + encodeURI(data),
-            //    //    target: '_blank',
-            //    //    download: 'filename.mp3'
-            //    //})[0].click();
-
-            //}).error(function (data, status, headers, config) {
-            //    // if there's an error you should see it here
-            //    console.log(data);
-            //});
-
-
-
-            //window.location.href = 'https://microsoft-apiapp55759bba47b74474bffa45d9538d840b.azurewebsites.net/api/download';
-            //$http({
-            //    method: 'POST',
-            //    url: 'https://microsoft-apiapp55759bba47b74474bffa45d9538d840b.azurewebsites.net/api/download'
-            //}).then(function successCallback(response) {
-            //    console.log(response)
-            //    // this callback will be called asynchronously
-            //    // when the response is available
-            //}, function errorCallback(response) {
-            //    // called asynchronously if an error occurs
-            //    // or server returns response with an error status.
-            //});
-        }
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/playlists');
     });
---
+++ www/js/controllers.js
@@ -0,0 +1,117 @@
+angular.module('starter.controllers', [])
+
+    .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
+
+        // Form data for the login modal
+        $scope.loginData = {};
+
+        // Create the login modal that we will use later
+        $ionicModal.fromTemplateUrl('templates/login.html', {
+            scope: $scope
+        }).then(function (modal) {
+            $scope.modal = modal;
+        });
+
+        // Triggered in the login modal to close it
+        $scope.closeLogin = function () {
+            $scope.modal.hide();
+        };
+
+        // Open the login modal
+        $scope.login = function () {
+            $scope.modal.show();
+        };
+
+        // Perform the login action when the user submits the login form
+        $scope.doLogin = function () {
+            console.log('Doing login', $scope.loginData);
+
+            // Simulate a login delay. Remove this and replace with your login
+            // code if using a login system
+            $timeout(function () {
+                $scope.closeLogin();
+            }, 1000);
+        };
+    })
+
+    .controller('PlaylistsCtrl', function ($scope, Items, $firebaseObject) {
+        $scope.playlists = Items;
+        $scope.download = function (ring) {
+            alert(ring);
+        }
+    })
+    .controller('BrowseCtrl', function ($scope, $http, $timeout, $upload, $cordovaFileTransfer, $ionicPlatform, $cordovaFileOpener2) {
+        $scope.upload = [];
+        $scope.fileUploadObj = { testString1: "Test string 1", testString2: "Test string 2" };
+
+        $scope.onFileSelect = function ($files) {
+            debugger;
+            //$files: an array of files selected, each file has name, size, and type.
+            for (var i = 0; i < $files.length; i++) {
+                var $file = $files[i];
+                (function (index) {
+                    $scope.upload[index] = $upload.upload({
+                        url: "https://microsoft-apiapp55759bba47b74474bffa45d9538d840b.azurewebsites.net/api/files/upload", // webapi url
+                        method: "POST",
+                        data: { fileUploadObj: $scope.fileUploadObj },
+                        file: $file
+                    }).progress(function (evt) {
+                        // get upload percentage
+                        console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                    }).success(function (data, status, headers, config) {
+                        // file is uploaded successfully
+                        console.log('Success');
+                        console.log(data);
+                    }).error(function (data, status, headers, config) {
+                        // file failed to upload
+                        console.log('Failure');
+                        console.log(data);
+                    });
+                })(i);
+            }
+        }
+        $scope.abortUpload = function (index) {
+            $scope.upload[index].abort();
+        }
+    })
+    .controller('PlaylistCtrl', function ($scope, $location, $stateParams, $cordovaFile, $cordovaFileTransfer, $cordovaFileOpener2, $timeout, $ionicLoading) {
+        $scope.tone = $location.path().split("/").pop();
+        $scope.download = function () {
+            debugger;
+            $ionicLoading.show();
+            document.addEventListener('deviceready', function () {
+
+                var url = "https://microsoft-apiapp55759bba47b74474bffa45d9538d840b.azurewebsites.net/api/tone?name=" + $scope.tone;
+                var targetPath = cordova.file.externalDataDirectory + $scope.tone;
+                var trustHosts = true;
+                var options = {};
+
+                $cordovaFile.checkFile(cordova.file.externalDataDirectory, $scope.tone)
+                    .then(function (success) {
+                        $ionicLoading.hide();
+                        $cordovaFileOpener2.open(targetPath, 'audio/mpeg').then(function () { }, function (err) { });
+                    }, function (error) {
+                        $cordovaFileTransfer.download(url, targetPath, options, trustHosts)
+                            .then(function (result) {
+                                $ionicLoading.hide();
+                                $cordovaFileOpener2.open(targetPath, 'audio/mpeg').then(function () { }, function (err) { });
+                            }, function (err) {
+                                console.log('Error');
+                                $ionicLoading.hide();
+
+                            }, function (progress) {
+                                $timeout(function () {
+                                    $scope.downloadProgress = (progress.loaded / progress.total) * 100;
+                                });
+                            });
+                    });
+            }, false);
+        }
+    });
